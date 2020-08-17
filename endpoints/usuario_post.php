70a274ac78d8b3a858d6c065c1e99533c6ac8d1d
Jadiê Oliveira <jadieoliveira00@gmail.com>
2020-08-17 09:28:35 -0300

    create user

--- endpoints/usuario_post.php
+++ endpoints/usuario_post.php
@@ -1,9 +1,29 @@
 <?
 function api_usuario_post($request) {
-  $response = array(
-    'nome' => 'Jadiê',
-    'email' => 'jadie@gmail.com'
-  );
+  $nome = sanitize_text_field($request['nome']);
+  $email = sanitize_email($request['email']); 
+  $senha = sanitize_text_field($request['senha']); 
+  // $rua = sanitize_text_field($request['rua']); 
+  // $cep = sanitize_text_field($request['cep']); 
+  // $numero = sanitize_text_field($request['numero']); 
+  // $bairro = sanitize_text_field($request['bairro']); 
+  // $cidade = sanitize_text_field($request['cidade']); 
+  // $estado = sanitize_text_field($request['estado']); 
+
+  $user_exists = username_exists($email);
+  $email_exists = email_exists($email);
+
+  if(!user_exists && !email_exists && $email && $senha){
+    $response = array(
+      'nome' => $nome,
+      'email' => $email,
+    );
+    wp_create_user($email, $senha, $email);
+
+  }else {
+    $response = new WP_Error('email', 'nem sei, só sei que deu nisso', array('status' => 403));
+  }
+
   return rest_ensure_response($response);
 }
 
